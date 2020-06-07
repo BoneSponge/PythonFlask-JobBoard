eca18df7ab38105005f8912fdc01e3c85c00408f
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2020-06-07 13:43:04 +0100

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -21,7 +21,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
     if commit is True:
         results = connection.commit()
     else:
-        results = cursor.fetchone() if single else curosr.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
     cursor.close()
     return results
 
